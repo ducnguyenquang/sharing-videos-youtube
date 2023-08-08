168cff5ec99fe325cced0bebe405abb2fce8fd2a
nguyenquangduczoitech <duc.nguyenquang@zoi.tech>
2023-08-08 13:04:57 +0700

    changed name of component

---
+++ components/Login/Login.test.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Login from '@/components/Login/Login';
+
+describe("Login Component", () => {
+  test("successful login", () => {
+    const mockOnLogin = jest.fn();
+    const mockOnLogout = jest.fn();
+    render(<Login onLogin={mockOnLogin} onLogout={mockOnLogout} />);
+
+    fireEvent.change(screen.getByPlaceholderText("email"), {
+      target: { value: "testuser" },
+    });
+    fireEvent.change(screen.getByPlaceholderText("password"), {
+      target: { value: "testpassword" },
+    });
+
+    fireEvent.click(screen.getByText("Login / Register"));
+
+    expect(mockOnLogin).toHaveBeenCalledTimes(1);
+    expect(mockOnLogin).toHaveBeenCalledWith("testuser");
+  });
+});
